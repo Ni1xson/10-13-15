a784ddf1e89435f5015ecc6857498b962d6ff512
NIXSON <sema.vedyagin@mail.ru>
2023-02-07 09:30:22 +0300

    first commit

---
+++ DZ/Задача 10/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трех значное число");
+int rendomNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Конечное число =>{rendomNumber}");
+int finishNumber = GetNumber(rendomNumber);
+Console.WriteLine($"Конечное число =>{finishNumber}");
+
+int GetNumber(int number)
+{
+
+int SecondNumber = number/10%10;
+
+
+int result=SecondNumber/10+SecondNumber;
+return result;
+}
+
+
+
---
+++ DZ/Задача 13/Program.cs
@@ -0,0 +1,17 @@
+﻿  int number = new Random().Next(1,10000000);
+Console.WriteLine($"рандом число =>{number}");
+    int sum = ngb(number);
+    Console.WriteLine(sum);
+   
+
+
+static int ngb(int i)
+{
+ 
+    while (i >= 1000) i /= 10;
+    int sum = i % 10;
+    return sum;
+
+    
+
+}
