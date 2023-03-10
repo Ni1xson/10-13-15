b97be0a1c96b2dc0b662608fae02e2bd73e7d93f
NIXSON <sema.vedyagin@mail.ru>
2023-03-10 11:28:50 +0300

    9lession

---
+++ DZ/Задание 64/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(1,20);
+		
+		Console.WriteLine();
---
+++ DZ/Задание 68/Program.cs
@@ -0,0 +1,16 @@
+﻿static uint Akk(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ 
+Console.WriteLine(Akk(2, 3)); 
+Console.WriteLine(Akk(0, 0)); 
+Console.WriteLine(Akk(4, 5)); 
+ 
+Console.ReadKey();
